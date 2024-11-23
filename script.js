377269565f5cc80db64b505cd8bab753bb6c855e
workdotnisha <workdotnisha@gmail.com>
2024-11-23 13:36:44 +0530

    Modifying files

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const button = document.querySelector('button');
+const shareButton = document.getElementById('share-button');
+const shareOptions = document.querySelector('.share-options');
+const shareImage = document.querySelector('.share-icon');
 
-button.addEventListener('click', () => {
-    button.style.backgroundColor = '#555A65';
-    button.style.color = '#ffffff';
-    button.appendChild(document.createElement('span')).textContent = 'Share';
+shareButton.addEventListener('click', () => {
+    if (shareOptions.style.display === 'none' || !shareOptions.style.display) {
+        shareOptions.style.display = 'flex'; 
+        shareButton.style.backgroundColor = '#555A65';
+        shareImage.style.filter = 'brightness(0) invert(1)';
+    } else {
+        shareOptions.style.display = 'none'; 
+        shareButton.style.backgroundColor = '#EDF2F8';
+        shareImage.style.filter = 'brightness(0) sepia(1) saturate(1) hue-rotate(180deg)';
+    }
 });
